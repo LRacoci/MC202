209360509c357cb800fbad61560b4a1c390e9c46
LRacoci <racoci.0@gmail.com>
2014-11-04 22:03:03 -0200

    afunilando acho

--- 202/Neucimar/7/testes/7.c
+++ 202/Neucimar/7/testes/7.c
@@ -25,10 +25,14 @@ typedef struct Subset{
 /************************************************/
 
 bool insere(Arvore * Raiz, int chave);
-bool insere_arv_bin(Arvore * Raiz, int chave);
+bool insere_arv_bin(Arvore * Raiz, int chave, NoArvBin** onde);
 void removE_arv_bin(Arvore * Raiz, int chave);
-void afunila(NoArvBin * f);
+bool afunila(NoArvBin * f);
 void rotacao(Arvore f);
+void liberaArvore(Arvore A);
+
+void pPre(Arvore A);
+void pIn(Arvore A);
 
 /* MEU PROGRAMA - MAIN */
 /**********************/
@@ -50,12 +54,15 @@ int main() {
 	for(i = 0; i < n; i++){
 		set[i].tam = 0;
 		set[i].elem = NULL;
-		scanf("s ");
+		scanf("\ns");
+		/***************************************************************************/
 		while((c = getchar()) != '\n'){
-			ungetc(c, stdin);
+			/*ungetc(c, stdin);*/
 			scanf("%d", &atual);
 			insere(&(set[i].elem), atual);
 			set[i].tam++;
+			pPre(set[i].elem); printf("\n");
+			pIn(set[i].elem); printf("\n\n");
 			if(set[i].tam > u){
 				printf("\033[91mERRO\033[97m\n");
 				exit(1);
@@ -63,7 +70,11 @@ int main() {
 		}
 	}
 
+	for(i = 0, i < n, i++)
+		liberaArvore(set[i].elem);
+	free(set);
 
+	return 0;
 
 } 
 /* IMPLEMENTAÇÃO DAS FUNÇÕES DE MINHA TAD */ 
@@ -72,27 +83,32 @@ int main() {
 /***************************************/
 
 bool insere(Arvore * Raiz, int chave){
-	bool inserivel = insere_arv_bin(Raiz, chave);
-	if(inserivel) afunila(Raiz);
-	return inserivel
+	NoArvBin *onde;
+	bool inserivel = insere_arv_bin(Raiz, chave, &onde);
+	if(inserivel) 
+		while(afunila(onde)){
+			*Raiz = onde;
+		}
+	return inserivel;
 }
 
-bool insere_arv_bin(Arvore * Raiz, int chave){
+bool insere_arv_bin(Arvore * Raiz, int chave, NoArvBin** onde){
 	if(*Raiz == NULL) {
 		*Raiz = (Arvore)malloc(sizeof(NoArvBin));
 		(*Raiz)->info = chave;
 		(*Raiz)->esq = NULL;
 		(*Raiz)->dir = NULL;
 		(*Raiz)->pai = NULL;
+		*onde = *Raiz;
 	}
 	else if(chave == (*Raiz)->info){
 		return false;
 	}
 	else if(chave < (*Raiz)->info){
-		insere_arv_bin(&(*Raiz)->esq, chave);
+		insere_arv_bin(&(*Raiz)->esq, chave, onde);
 		(*Raiz)->esq->pai = (*Raiz);
 	}else{
-		insere_arv_bin(&(*Raiz)->dir, chave);
+		insere_arv_bin(&(*Raiz)->dir, chave, onde);
 		(*Raiz)->dir->pai = (*Raiz);
 	}
 	return true;
@@ -100,16 +116,16 @@ bool insere_arv_bin(Arvore * Raiz, int chave){
 void removE_arv_bin(Arvore * Raiz, int chave){
 	
 }
-void afunila(NoArvBin * f){
+bool afunila(NoArvBin * f){
 	NoArvBin *p, *a;
-	if (f == NULL) return;
+	if (f == NULL) return false;
 	else p = f->pai;
 	
-	if(p == NULL) return;
+	if(p == NULL) return false;
 	else a = p->pai;
 
 	if(a == NULL){
-		rotacao(p);
+		rotacao(f);
 	}else{
 		/* Se for homagêneo */
 		if((a->dir == p && p->dir == f) || (a->esq == p && p->esq == f)){
@@ -120,17 +136,42 @@ void afunila(NoArvBin * f){
 			rotacao(f);
 		}
 	}
+	return true;
 }
 
-void rotacao(Arvore f) {
-	Arvore p = f->pai;
+void rotacao(NoArvBin * f) {
 	/* Nesse caso f é o filho direito */
-	if(f->pai->dir == f){
+	if(f->pai->esq == f){
 		f->pai->esq = f->dir;
 		f->dir = f->pai;
+		f->pai = f->dir->pai;
+		f->dir->pai = f;
 	}else{
 		f->pai->dir = f->esq;
 		f->esq = f->pai;
+		f->pai = f->esq->pai;
+		f->esq->pai = f;
 	}
 }
-
+void liberaArvore(Arvore A){
+	if(A){
+		liberaArvore(A->dir);
+		liberaArvore(A->esq);
+		free(A);
+	}
+}
+void pPre(Arvore A){
+	if(A){
+		printf("%d ", A->info);
+		pPre(A->esq);
+		pPre(A->dir);
+	}
+}
+void pIn(Arvore A){
+	if(A){
+		pIn(A->esq);
+		printf("%d ", A->info);
+		pIn(A->dir);
+	}
+	else printf("* ");
+}
